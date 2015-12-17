403212a27a95328762b38d0323082dac36560784
Dave Wasmer <davewasmer@gmail.com>
2015-12-16 20:15:59 -0700

    Remove brocfile, fix waypoint import

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/waypoints/lib/noframework.waypoints.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
